d766bcb0346b486c7265615d6fb9c07f631e1752
ChristopherNClark <chrclark@fiu.edu>
2024-01-05 10:47:55 -0500

    Added data for weeks 4-8.

---
+++ R/data_week5.R
@@ -0,0 +1,14 @@
+#' Week 5 Household Pulse Survey data
+#'
+#' Week 5 data from the US census's Household Pulse Survey.
+#'
+#'
+#' @format ## `data_week5`
+#' A data frame with 105,066 rows and 82 columns:
+#' \describe{
+#'   \item{SCRAM}{Response ID}
+#'   \item{WEEK}{Week Number}
+#'   ...
+#' }
+#' @source <https://github.com/prasadbhoite/HPS_PUF/raw/main/Data>
+"data_week5"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -37,3 +37,54 @@ save(data_week3, file = "data/data_week3.rda", compress='xz')
 usethis::use_r("data_week3")
 #devtools::document()
 
+# Week 4:
+parquet_url_week4 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/04.Week4_05.21.20_05.26.20/HPS_week4_data_parquet.parquet"
+# Read the Parquet file into a data frame
+data_week4 <- arrow::read_parquet(parquet_url_week4)
+save(data_week4, file = "data/data_week4.rda", compress='xz')
+
+#Document the Week 4 dataset below:
+usethis::use_r("data_week4")
+#devtools::document()
+
+
+# Week 5:
+parquet_url_week5 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/05.Week5_05.28.20_06.02.20/HPS_week5_data_parquet.parquet"
+# Read the Parquet file into a data frame
+data_week5 <- arrow::read_parquet(parquet_url_week5)
+save(data_week5, file = "data/data_week5.rda", compress='xz')
+
+#Document the Week 5 dataset below:
+usethis::use_r("data_week5")
+#devtools::document()
+
+# Week 6:
+parquet_url_week6 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/06.Week6_06.04.20_06.09.20/HPS_week6_data_parquet.parquet"
+# Read the Parquet file into a data frame
+data_week6 <- arrow::read_parquet(parquet_url_week6)
+save(data_week6, file = "data/data_week6.rda", compress='xz')
+
+#Document the Week 6 dataset below:
+usethis::use_r("data_week6")
+#devtools::document()
+
+
+# Week 7:
+parquet_url_week7 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/07.Week7_06.11.20_06.16.20/HPS_week7_data_parquet.parquet"
+# Read the Parquet file into a data frame
+data_week7 <- arrow::read_parquet(parquet_url_week7)
+save(data_week7, file = "data/data_week7.rda", compress='xz')
+
+#Document the Week 7 dataset below:
+usethis::use_r("data_week7")
+#devtools::document()
+
+# Week 8:
+parquet_url_week8 <- "https://github.com/prasadbhoite/HPS_PUF/raw/main/Data/08.Week8_06.18.20_06.23.20/HPS_week8_data_parquet.parquet"
+# Read the Parquet file into a data frame
+data_week8 <- arrow::read_parquet(parquet_url_week8)
+save(data_week8, file = "data/data_week8.rda", compress='xz')
+
+#Document the Week 8 dataset below:
+usethis::use_r("data_week8")
+#devtools::document()
